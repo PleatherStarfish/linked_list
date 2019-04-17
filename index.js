169ec0c1125584e1b3706459924ca2f3148a961e
Daniel Miller <lontano.composer@gmail.com>
2019-04-17 16:41:36 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { LinkedList, Node } = require("./list.js");
+
+
+let list = new LinkedList();
+
+list.addLast("one");
+list.addLast("two");
+list.addLast("three");
+list.addLast("four");
+list.addLast("five");
+list.addLast("six");
+list.addLast("seven");
+list.addLast("eight");
+list.addFirst("minus-one");
+
+list.printList();
---
+++ list.js
@@ -0,0 +1,62 @@
+"use strict";
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data,
+        this.next = next
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    get length() {
+        return this.count;      // Returns length of linked list
+    }
+
+    get isEmpty() {
+        return this.size === 0; // Returns a boolean in answer to query
+    }
+
+    printList() {
+        let current = this.head;
+        while (current.next) {
+            console.log(current);
+            current = current.next;
+        }
+    }
+
+    addFirst(data) {
+        const node = new Node(data); // Create new node
+        if (this.head === null) {
+            this.head = node;         // If the list is headless, make new node the head
+        }
+        else {
+            let current = this.head;
+            this.head = node;
+            this.head.next = current;
+        }
+        this.count++;
+    }
+
+    addLast(data) {
+        let node = new Node(data);    // Create a new node
+        if (this.head === null) {
+            this.head = node;         // If the list is headless, make new node the head
+        }
+        else {
+            let current = this.head;
+            while (current.next) { current = current.next; } // Otherwise iterate to end of list and add node
+            current.next = node;
+        }
+        this.count++;
+    }
+}
+
+module.exports = {
+    LinkedList,
+    Node
+}
